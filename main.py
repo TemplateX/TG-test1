ddd797d98c5a9af9f57a9e1fa8f45fa2289126a2
TemplateX <160799313+TemplateX@users.noreply.github.com>
2024-02-22 03:26:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,62 @@
 print('qq1')
+from keep_alive import keep_alive
+
 #telegram
-#import asyncio
 from pyrogram import Client, filters
+import pytz
+import datetime
+import time
 # 51.79.229.202:3128
 import os
+
 api_id = str(os.environ['s_api_id'])
 api_hash = (os.environ['s_api_hash'])
-app = Client("my_account", api_id=api_id, api_hash=api_hash)
-session_string = 'AgFBU0kAdqeZ5dyes4h16vy3KxMrOEsBX2Z4ulHPBwXYiFLnciqL6IM0azIo8iAITv8XpOoKo7K6pGf07f4bOADHkay2ln3n3hqymAx2UZ9sUffSfrhKnhX5NwtjyxLihXirjNXV4WJkCgNb-_p7x1JczV6taO0YQr04gW31C9GGDjZ8PLAdhz7Hayxdcruwska0iZACNt6yezlyPzp5vcsn-H_IiFjp88LPAGGBgCWT-IANUMLDg47FUzqf6AIPN8R45htteazWOk8iGODclrti3yIqXdpBO3DgTDc13W3nWnfZEZhozwc8momdZwefYIo9xP1Y09vekiwSkPlRsS5lLVoTpQAAAAA3cDxDAA'
+app = Client("my_account", api_id, api_hash)
+print('qq:')
 
-async def main():
-  async with Client("my_account", session_string=session_string) as app:
-      #print(await app.export_session_string())
-      print('inside_qq')
-      print(await app.get_me())
 
-app.run(main())  # Automatically start() and idle()
+PUBLIC_FROM = [
+    # список пабликов-доноров, откуда бот будет пересылать посты
+    'tass_agency',
+    'rbc_news',
+    'breakingmash',
+    'svtvnews',
+    'meduzalive',
+    'Ateobreaking',
+    'SolovievLive',
+    'news_sirena',
+    'bbbreaking',
+    'rusbrief',
+    'yxoydo6si6e7474uf'#Мой скрытый канал
+    #"-1002047800128" #Template - мой канал id,
+]
+
+
+@app.on_message(filters.chat(PUBLIC_FROM))
+def main(Client, message):
+    finale = message.text or message.caption
+    if finale is None:
+      print('-------finale is None!')
+      print('Там цитата или что-то ещё')
+      print(message.link)
+    else:
+      #print(finale)
+      if message.chat.title is not None:
+        tz_moscow = pytz.timezone("Europe/Moscow")
+        dt_moscow = str(datetime.datetime.now(tz_moscow).strftime("%d.%m.%Y_%H:%M:%S"))
+        MyMessageTitle = message.chat.title
+        finale = '#' + str(MyMessageTitle) + ' ' + str(dt_moscow) + '\n\n' + finale
+        # aMessageFromNone = message.forward_from_chat.title
+        if message.forward_from_chat is not None:
+            MyMessageTitle2 = message.forward_from_chat.title
+            # finale = str(MyMessageTitle) + '\n' + "Переслал из:" + '\n' + str(MyMessageTitle2) + '\n\n' + finale
+            finale = finale + '\n\n' + '- * - * - * - * - * - * -\n' + str(MyMessageTitle) + " Переслал из: " + '#'+ str(
+                MyMessageTitle2)
+      else:
+        print('-------message.chat.title is None')
+      print(finale)
+      app.send_message(chat_id='hrdshs00rhsge36w2546', text=finale)
+
+
+# keep_alive()
+app.run()  # Automatically start() and idle()
